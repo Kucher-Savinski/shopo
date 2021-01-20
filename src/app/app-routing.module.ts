127500f539939e0740e70837d6c91ca4da2c71e0
Kucher-Savinski <n240102@icloud.com>
2021-01-20 21:34:19 +0200

    cod-new

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './common/shared/main-layout/main-layout.component';
+import { MainPageComponent } from './common/main-page/main-page.component';
+import { ProductPageComponent } from './common/product-page/product-page.component';
+import { CartPageComponent } from './common/cart-page/cart-page.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: MainPageComponent},
+      {path: 'product/:id', component: ProductPageComponent},
+      {path: 'cart', component: CartPageComponent}
+    ]
+  },
+  {
+    path: 'admin', loadChildren: './admin/admin.module#AdminModule'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './common/shared/main-layout/main-layout.component';
+import { MainPageComponent } from './common/main-page/main-page.component';
+import { ProductPageComponent } from './common/product-page/product-page.component';
+import { CartPageComponent } from './common/cart-page/cart-page.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    CartPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
